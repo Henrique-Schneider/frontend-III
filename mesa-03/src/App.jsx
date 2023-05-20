1a9b451be25669c034ec54ebb56bd82d10e3125c
Henrique-Schneider <domeneider@gmail.com>
2023-05-20 20:05:47 -0300

    mesa-03

--- mesa-03/src/App.jsx
+++ mesa-03/src/App.jsx
@@ -1,22 +1,15 @@
-import { useState } from 'react'
 import './App.css'
+import HelloWorldChildren from './components/HelloWorldChildren'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      
-      <h1>Ola, mundo!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
+      <div>
+        <HelloWorldChildren name={"Henrique"}>Hello</HelloWorldChildren>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
---
+++ mesa-03/src/components/HelloWorldChildren.jsx
@@ -0,0 +1,10 @@
+import './HelloWorldChildren.css'
+function HelloWorldChildren({name,children}) {
+  return ( 
+    <div>
+      {children}, {name}
+    </div>
+   );
+}
+
+export default HelloWorldChildren;
